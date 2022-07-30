27bf2f1052574c1cb07c0c7f062a93cf878816e3
monalisamsteccentric <monalisa.nextattempt@gmail.com>
2022-07-30 14:58:27 +0530

    my second commit

--- src/App.js
+++ src/App.js
@@ -1,8 +1,6 @@
 import React from 'react'
 import Home from "./pages/home/Home";
-import Login from './pages/Login/Login';
-import Profile from './pages/Profile/Profile';
-import Register from './pages/Register/Register';
+
 
 
 
--- src/pages/Profile/Profile.jsx
+++ src/pages/Profile/Profile.jsx
@@ -3,7 +3,6 @@ import './profile.css'
 import Feed from "../../components/feed/Feed"
 import Rightbar from "../../components/rightbar/Rightbar"
 import Sidebar from "../../components/sidebar/Sidebar"
-import Topbar from "../../components/topbar/Topbar"
 import imagewoman from '../../components/Assets/woman-image.jpg';
 
 const Profile = () => {
